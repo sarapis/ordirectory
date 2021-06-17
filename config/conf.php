967c62efc20a6a03f2386de44b998954497d068a
Maxim Pokrovsky <maxim@soundpress.biz>
2021-06-18 02:01:37 +0300

    cronjob packages

--- config/conf.php
+++ config/conf.php
@@ -4,8 +4,8 @@ return [
 
 	/* API entry point */
     #'APIENTRY' => 'http://devinfusiodc',
-    'APIENTRY' => 'http://devinfusiodev2',
-    #'APIENTRY' => 'http://18.219.50.199',
+    #'APIENTRY' => 'http://devinfusiodev2',
+    'APIENTRY' => 'http://18.219.50.199',
 	
 	/* Last data update date (is displaying at the bottom of search form) */
     #'DATA_ACTUAL_DATE' => 'November 1st 2019',
