03238e7cf42ffa3f7372dd80c8f871a4261af416
Maxim Pokrovsky <maxim@soundpress.biz>
2021-10-07 01:05:48 +0300

    DC ors.sarapis.org update Initial

--- resources/views/service.blade.php
+++ resources/views/service.blade.php
@@ -100,7 +100,7 @@
 						var mapnik         = new OpenLayers.Layer.OSM();
 						var fromProjection = new OpenLayers.Projection("EPSG:4326");   // Transform from WGS 1984
 						var toProjection   = new OpenLayers.Projection("EPSG:900913"); // to Spherical Mercator Projection
-						var position       = new OpenLayers.LonLat({{ (array)$data['Lng'][0] }},{{ (array)$data['Lat'][0] }}).transform( fromProjection, toProjection);
+						var position       = new OpenLayers.LonLat({{ (array)($data['Lng'])[0] }},{{ (array)($data['Lat'])[0] }}).transform( fromProjection, toProjection);
 						var zoom           = 15; 
 
 						map.addLayer(mapnik);
