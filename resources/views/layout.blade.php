8a93ad4e3f3c7cea209373de3cbd63390705b9b3
Maxim Pokrovsky <maxim@soundpress.biz>
2021-10-13 02:32:08 +0300

    ORservices export dataset adopted v1.0

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -30,7 +30,12 @@
 			{{ $style['selector'] }} {{!! $style['style'] !!}}
 		@endforeach
 	</style>
-	<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=61660f404564d200122a7def&product=sop' async='async'></script>
+	<!--<script type='text/javascript' src='https://platform-api.sharethis.com/js/sharethis.js#property=61660f404564d200122a7def&product=sop' async='async'></script>-->
+	 <script
+      type="text/javascript"
+      src="https://platform-api.sharethis.com/js/sharethis.js#property=5f2ae272de3ff522686e1f6e&product=sop"
+      async="async"
+    ></script>
 	<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 </head>
 
